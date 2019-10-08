a3b8b821d1f7d72545e7db5dfedbd1f0cd85e0b3
Timothy Woo <twoocoder88@gmail.com>
2019-10-07 18:19:01 -0700

    create simple calc ui; add & pass initial tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import StringCalculator from './components/string-calculator'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<StringCalculator />, document.getElementById('root'))
